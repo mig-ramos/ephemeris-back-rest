176df02f64693da0e107f1da4d62d24d6e66cf11
Miguel Arcanjo <mig.ramos10@gmail.com>
2021-02-13 20:35:27 -0300

    Testando a primenra classe de dominio

--- src/main/java/com/siderbit/ephemeris/resources/EstadoRecource.java
+++ src/main/java/com/siderbit/ephemeris/resources/EstadoRecource.java
@@ -1,15 +1,27 @@
 package com.siderbit.ephemeris.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.siderbit.ephemeris.domains.Estado;
 
 @RestController
 @RequestMapping(value="/estados")
 public class EstadoRecource {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "REST está funcionando!";
+	public List<Estado> listar() {
+		Estado e1 = new Estado(1, "São Paulo");
+		Estado e2 = new Estado(2, "Paraná");
+		
+		List<Estado> lista = new ArrayList<>();
+		lista.add(e1);
+		lista.add(e2);
+		
+		return lista;
 	}
 }
