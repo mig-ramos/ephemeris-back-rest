1b95dd250a315a952e2d11d25a1504f0645a7fde
Miguel Arcanjo <mig.ramos10@gmail.com>
2021-02-13 19:11:27 -0300

    Projeto criado

---
+++ src/main/java/com/siderbit/ephemeris/EphemerisApplication.java
@@ -0,0 +1,13 @@
+package com.siderbit.ephemeris;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EphemerisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EphemerisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/siderbit/ephemeris/EphemerisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.siderbit.ephemeris;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EphemerisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
