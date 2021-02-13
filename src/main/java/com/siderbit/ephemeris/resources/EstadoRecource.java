db5f1255084860a2931caaff229046e0bc739132
Miguel Arcanjo <mig.ramos10@gmail.com>
2021-02-13 19:51:03 -0300

    REST testado

---
+++ src/main/java/com/siderbit/ephemeris/resources/EstadoRecource.java
@@ -0,0 +1,15 @@
+package com.siderbit.ephemeris.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/estados")
+public class EstadoRecource {
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public String listar() {
+		return "REST está funcionando!";
+	}
+}
