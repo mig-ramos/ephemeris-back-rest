2706c619a77a5dc79a282488de7ec9ff242d81d8
Miguel Arcanjo <mig.ramos10@gmail.com>
2021-02-23 14:08:54 -0300

    Deletando uma Especialidade com DELETE

--- src/main/java/com/siderbit/ephemeris/services/EspecialidadeService.java
+++ src/main/java/com/siderbit/ephemeris/services/EspecialidadeService.java
@@ -3,10 +3,12 @@ package com.siderbit.ephemeris.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.siderbit.ephemeris.domains.Especialidade;
 import com.siderbit.ephemeris.repositories.EspecialidadeRepository;
+import com.siderbit.ephemeris.services.exceptions.DataIntegrityException;
 import com.siderbit.ephemeris.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -29,5 +31,15 @@ public class EspecialidadeService {
 	public Especialidade update(Especialidade obj) {
 		find(obj.getId());
 		return repo.save(obj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma especialidade que possui produtos");
+		}
 	}
 }
