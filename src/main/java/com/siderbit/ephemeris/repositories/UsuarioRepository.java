e103460f95c22ec7a31a17b0bf5caedcbb283b4e
Miguel Arcanjo <mig.ramos10@gmail.com>
2021-02-25 16:24:41 -0300

    Validacao de email nao repetido na insercao do Usuario

--- src/main/java/com/siderbit/ephemeris/repositories/UsuarioRepository.java
+++ src/main/java/com/siderbit/ephemeris/repositories/UsuarioRepository.java
@@ -2,10 +2,12 @@ package com.siderbit.ephemeris.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.siderbit.ephemeris.domains.Usuario;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
-
+	@Transactional(readOnly=true)
+	Usuario findByEmail(String email);
 }
--- src/main/java/com/siderbit/ephemeris/services/validation/UsuarioInsertValidator.java
+++ src/main/java/com/siderbit/ephemeris/services/validation/UsuarioInsertValidator.java
@@ -6,12 +6,18 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.siderbit.ephemeris.domains.Usuario;
 import com.siderbit.ephemeris.dto.UsuarioNewDTO;
+import com.siderbit.ephemeris.repositories.UsuarioRepository;
 import com.siderbit.ephemeris.resources.exceptions.FieldMessage;
 
 
 public class UsuarioInsertValidator implements ConstraintValidator<UsuarioInsert, UsuarioNewDTO> {
 
+	@Autowired
+	private UsuarioRepository repo;
 	@Override
 	public void initialize(UsuarioInsert ann) {
 	}
@@ -20,6 +26,11 @@ public class UsuarioInsertValidator implements ConstraintValidator<UsuarioInsert
 	public boolean isValid(UsuarioNewDTO objDto, ConstraintValidatorContext context) {
 
 		List<FieldMessage> list = new ArrayList<>();
+		
+		Usuario aux = repo.findByEmail(objDto.getEmail());
+		if (aux != null) {
+			list.add(new FieldMessage("email", "Email já existente"));
+		}
 
 		for (FieldMessage e : list) {
 			context.disableDefaultConstraintViolation();
