0b6e84be092b6ec16ee38849115528eed3efe514
Miguel Arcanjo <mig.ramos10@gmail.com>
2021-03-02 11:26:27 -0300

    Implementando autorizacao

--- src/main/java/com/siderbit/ephemeris/config/SecurityConfig.java
+++ src/main/java/com/siderbit/ephemeris/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.siderbit.ephemeris.security.JWTAuthenticationFilter;
+import com.siderbit.ephemeris.security.JWTAuthorizationFilter;
 import com.siderbit.ephemeris.security.JWTUtil;
 
 @Configuration
@@ -36,7 +37,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	private static final String[] PUBLIC_MATCHERS = { "/h2-console/**" };
 
-	private static final String[] PUBLIC_MATCHERS_GET = { "/especialidades/**", "/estados/**", "/usuarios/**" };
+	private static final String[] PUBLIC_MATCHERS_GET = { "/especialidades/**", "/estados/**"};
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
@@ -52,6 +53,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.antMatchers(PUBLIC_MATCHERS).permitAll()
 			.anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 
