f492b5702e333d950e4ced2121f1a9b8e708d1ea
Miguel Arcanjo <mig.ramos10@gmail.com>
2021-03-05 14:41:54 -0300

    Bloqueio de autorizacao para Incluir, Alterar e Deletar Horas para Pacientes

--- src/main/java/com/siderbit/ephemeris/resources/HoraResource.java
+++ src/main/java/com/siderbit/ephemeris/resources/HoraResource.java
@@ -8,6 +8,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -32,6 +33,7 @@ public class HoraResource {
 		return ResponseEntity.ok().body(obj);
 	}
 
+	@PreAuthorize("hasAnyRole('ADMIN')")
 	@RequestMapping(method = RequestMethod.POST)
 	public ResponseEntity<Void> insert(@Valid @RequestBody HoraDTO objDto) {
 		Hora obj = service.fromDTO(objDto);
@@ -40,6 +42,7 @@ public class HoraResource {
 		return ResponseEntity.created(uri).build();
 	}
 
+	@PreAuthorize("hasAnyRole('ADMIN')")
 	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
 	public ResponseEntity<Void> update(@Valid @RequestBody HoraDTO objDto, @PathVariable Integer id) {
 		Hora obj = service.fromDTO(objDto);
@@ -48,6 +51,7 @@ public class HoraResource {
 		return ResponseEntity.noContent().build();
 	}
 
+	@PreAuthorize("hasAnyRole('ADMIN')")
 	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
 	public ResponseEntity<Void> delete(@PathVariable Integer id) {
 		service.delete(id);
